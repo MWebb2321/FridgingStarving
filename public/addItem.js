ecb7ac479badd2cf724a3e21750af4eca0346283
Matthew Webb <mwebb2321@gmail.com>
2021-04-08 19:54:33 -0700

    Hopefully this allows us to add dishes

--- public/addItem.js
+++ public/addItem.js
@@ -1,28 +1,49 @@
 const itemAdd = async (event) => {
-    // Stop the browser from submitting the form so we can do so with JavaScript
-    event.preventDefault();
-  
-    // Gather the data from the form elements on the page
-    const item = document.querySelector('#item-add').value.trim();
-  
-    if (item) {
-      // Send the e-mail and password to the server
-      const response = await fetch('/api/fridge/name', {
-        method: 'POST',
-        body: JSON.stringify({ name }),
-        headers: { 'Content-Type': 'application/json' },
-      });
-  
-      if (response.ok) {
-        document.location.replace('/');
-      } else {
-        alert('Failed to add item');
-      }
+  const dishId = event.target.value;
+
+  // Stop the browser from submitting the form so we can do so with JavaScript
+  event.preventDefault();
+
+  const response = await fetch("/api//${dishId}");
+  const dishInfo = await response.json();
+  if (response.ok) {
+    const postResponse = await fetch("/api/fridge/name", {
+      method: "POST",
+      body: JSON.stringify({ name }),
+      headers: { "Content-Type": "application/json" },
+    });
+
+    if (postResponse.ok) {
+      alert("Dish Added");
+    } else {
+      alert("Failed to add dish");
     }
-  };
-  
-  
-  document
-    .querySelector('.login-form')
-    .addEventListener('submit', itemAdd);
-  
+  } else {
+    alert("Could not add dish");
+  }
+};
+
+var addDishButton = document.getElementsByClassName("add-dish-button");
+for (var i = 0; i < addDishButton.length; i++) {
+  addDishButton[i].addEventListener("click", addDish);
+}
+
+// Gather the data from the form elements on the page
+const item = document.querySelector("#item-add").value.trim();
+
+if (item) {
+  // Send the e-mail and password to the server
+  const response = await fetch("/api/fridge/name", {
+    method: "POST",
+    body: JSON.stringify({ name }),
+    headers: { "Content-Type": "application/json" },
+  });
+
+  if (response.ok) {
+    document.location.replace("/");
+  } else {
+    alert("Failed to add item");
+  }
+}
+
+document.querySelector(".login-form").addEventListener("submit", itemAdd);
