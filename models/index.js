189c28c772066f3e9083efd67f59a5a4b6d4186f
Matthew Webb <mwebb2321@gmail.com>
2021-04-08 19:37:10 -0700

    added foreignKeys

--- models/index.js
+++ models/index.js
@@ -1,4 +1,12 @@
-const User = require('./User');
-const Fridge = require('./Fridge')
+const User = require("./User");
+const Fridge = require("./Fridge");
+
+User.hasMany(Dishes_Owned, {
+  foreignKey: "user_id",
+});
+
+Fridge.belongsTo(User, {
+  foreignKey: "user_id",
+});
 
 module.exports = { User, Fridge };
